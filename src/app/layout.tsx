3d48ce3e67e4281e06846081ba399669edf20d8f
shourov-rahman <shourovju43@gmail.com>
2024-08-07 01:57:25 +0600

    refactor: rename the folder name "providers" and updtate path based on "providers" folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ReactQueryProvider from "@/components/prividers/ReactQueryProvider";
+import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
